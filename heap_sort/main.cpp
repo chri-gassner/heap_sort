b362261c9a2d4a52746019ed3fec5d08dc70be42
Christoph Gassner <christoph@braurup.at>
2022-10-10 11:52:58 +0200

    Initial Commit

---
+++ heap_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  heap_sort
+//
+//  Created by Christoph Gassner on 10.10.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
