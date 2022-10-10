f0c58af11cb8996f3c9494b2090cafb17053ae5a
Christoph Gassner <christoph@braurup.at>
2022-10-10 11:54:22 +0200

    go

---
+++ heap_sort/heap_sort.cpp
@@ -0,0 +1,8 @@
+//
+//  heap_sort.cpp
+//  heap_sort
+//
+//  Created by Christoph Gassner on 10.10.22.
+//
+
+#include "heap_sort.hpp"
---
+++ heap_sort/heap_sort.hpp
@@ -0,0 +1,13 @@
+//
+//  heap_sort.hpp
+//  heap_sort
+//
+//  Created by Christoph Gassner on 10.10.22.
+//
+
+#ifndef heap_sort_hpp
+#define heap_sort_hpp
+
+#include <stdio.h>
+
+#endif /* heap_sort_hpp */
